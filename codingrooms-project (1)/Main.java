be8d651480e7d26949e776118008bd35ae16e0e5
OU4583 <99269225+OU4583@users.noreply.github.com>
2024-01-05 09:21:19 -0500

    Add files via upload

---
+++ codingrooms-project (1)/Main.java
@@ -0,0 +1,11 @@
+//Oscar Umana
+// APCS
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    TextAdventure adventure = new TextAdventure();
+    adventure.play();
+    // new game
+  }
+}
